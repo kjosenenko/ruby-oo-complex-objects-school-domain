d2c20818faf080523945cfd68f17ad6ec8dabcb4
KJ Osenenko <karljosenenko@gmail.com>
2020-11-17 14:36:54 -0700

    commit tree 8ff623826ae49cbabe2a8c5b492b7b660f7946f0

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,39 @@
+require "pry"
+
 # code here!
+class School
+    # attr_accessor :student, :grade
+    attr_reader :school
+
+    def initialize(school)
+        @school = school
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(student, grade)
+        if !@roster[grade]
+            @roster[grade] = []
+        end
+        @roster[grade] << student
+        
+        # binding.pry
+        # @@roster = @grade
+        # puts @grade
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        hash = {}
+        @roster.each do |grade, students| 
+            hash[grade] = students.sort
+        end
+        hash
+    end
+end
